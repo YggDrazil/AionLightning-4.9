8e601dbd70d7bd5e5894c9244930a017edba0e4f
YggDrazil <yggdrazil@hotmail.de>
2016-06-16 06:38:48 +0200

    Update PanelCS.bat

--- AL-Chat/dist/PanelCS.bat
+++ AL-Chat/dist/PanelCS.bat
@@ -1,25 +1,25 @@
 @ECHO off
-TITLE Aion Lightning - Chat Server Panel
+TITLE AionReborn - CommunityServer Monitor
 :MENU
 CLS
 ECHO.
 ECHO   ^*--------------------------------------------------------------------------^*
-ECHO   ^|                    Aion Lightning - Chat Server Panel                    ^|
+ECHO   ^|                    AionReborn - CommunityServer Monitor                  ^|
 ECHO   ^*--------------------------------------------------------------------------^*
 ECHO   ^|                                                                          ^|
-ECHO   ^|    1 - Development                                       3 - Quit        ^|
-ECHO   ^|    2 - Production                                                        ^|
+ECHO   ^|    1 - Entwicklung                                       3 - Beenden     ^|
+ECHO   ^|    2 - Betrieb                                                           ^|
 ECHO   ^|                                                                          ^|
 ECHO   ^*--------------------------------------------------------------------------^*
 ECHO.
-SET /P OPTION=Type your option and press ENTER: 
+SET /P OPTION=Triff deine Auswahl und druecke ENTER: 
 IF %OPTION% == 1 (
-SET MODE=DEVELOPMENT
+SET MODE=Entwicklung
 SET JAVA_OPTS=-Xms256m -Xmx512m -Xdebug -Xrunjdwp:transport=dt_socket,address=8997,server=y,suspend=n -ea
 CALL StartCS.bat
 )
 IF %OPTION% == 2 (
-SET MODE=PRODUCTION
+SET MODE=Betrieb
 SET JAVA_OPTS=-Xms512m -Xmx1024m -client
 CALL StartCS.bat
 )
